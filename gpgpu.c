7e4d868aa12e90ba7a2bd6fbab404809c4172dba
Qruliq <45049467+QruliQk@users.noreply.github.com>
2026-01-20 18:27:45 +0100

    Update gpgpu.c

--- gpgpu.c
+++ gpgpu.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include "header_files/gpgpu.h"
 
+/* Brak CUDA: wersja CPU algorytmu LU  */
 void GPGPU(double* A, double* L, double* U, int n)
 {
-    (void)A; (void)L; (void)U; (void)n;
+    fprintf(stderr,
+        "\nGPGPU niedostepne: brak dzialajacego CUDA device.\n"
+        "Brak CUDA: wersja CPU algorytmu LU. \n\n");
 
-    printf("\nGPGPU Niedostepne: brak CUDA (nvcc) lub brak kompatybilnego GPU NVIDIA.\n");
-    printf("Aby wlaczyc GPGPU zainstaluj CUDA Toolkit i skompiluj na maszynie z NVIDIA.\n\n");
+    for (int k = 0; k < n; k++) {
+
+        /* wiersz U[k][j] */
+        for (int j = k; j < n; j++) {
+            double sum = 0.0;
+            for (int m = 0; m < k; m++) {
+                sum += L[k * n + m] * U[m * n + j];
+            }
+            U[k * n + j] = A[k * n + j] - sum;
+        }
+
+        /* kolumna L[i][k] */
+        for (int i = k + 1; i < n; i++) {
+            double sum = 0.0;
+            for (int m = 0; m < k; m++) {
+                sum += L[i * n + m] * U[m * n + k];
+            }
+            L[i * n + k] = (A[i * n + k] - sum) / U[k * n + k];
+        }
+    }
 }
-
